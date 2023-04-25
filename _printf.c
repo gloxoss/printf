2b2197c8666d20ebd74c7dec83b105e1307907e1
gloxoss <gloxoss@gmail.com>
2023-04-25 13:22:26 -0700

    h

--- _printf.c
+++ _printf.c
@@ -1,58 +1,73 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
-/**
- * _putchar - writes a character to stdout
- * @c: the character to write
- *
- * Return: On success 1, on error -1.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+int whitespaces(const char *format, int *i);
 
 /**
- * _printf - prints a formatted string to stdout
- * @format: the format string
+ * _printf - prints a formatted string to stdout, similar to printf.
+ * @format: the format of the string to be printed.
  *
- * Return: the number of characters printed
+ * This function prints a formatted string to the stdout stream. It
+ * accepts a format string as its first argument and any additional arguments
+ * will be used to replace format specifiers in the format string. The function
+ *
+ * Return: the number of characters printed to the stdout stream.
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
+	va_list args, args_copy;
+	flags_t flags = {0};
+	int (*pfn)(va_list);
+	int i = 0, printed = 0, num;
 
-va_start(args, format);
+	if (!format)
+		return (-1);
+	va_start(args, format);
+	va_copy(args_copy, args);
+	for (; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[++i] == '\0')
+				return (-1);
+			/* flag handler */
+			num = va_arg(args_copy, long);
+			parse_flags(format, &flags, num, &printed, &i);
+			/* handle field width */
+			/*
+			if (_isdigit(format[i + 1]))
+			{
+				field_width = format[i + 1] - '0';
+				for (j = i + 2; _isdigit(format[j]); j++)
+					field_width = field_width * 10 + (format[j] - '0');
+				i = j - 1;
+			}
+			*/
+			if (!whitespaces(format, &i))
+				return (-1);
+			pfn = get_print(&format[i]);
+			/* for invalid formats: print as is */
+			printed += pfn ? pfn(args) : _putchar('%') + _putchar(format[i]);
+		}
+		else
+			printed += _putchar(format[i]);
+	}
+	va_end(args);
+	va_end(args_copy);
+	return (printed);
+}
 
-while (*format != '\0')
+/**
+ * whitespaces - Skip whitespaces in a string
+ * @format: The string to be evaluated
+ * @i: Pointer to the index of the character being evaluated
+ * Return: 1 if valid specifer, 0 otherwise
+ */
+int whitespaces(const char *format, int *i)
 {
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-_putchar(va_arg(args, int));
-count++;
-break;
-case 's':
-count += _printf(va_arg(args, char *));
-break;
-case '%':
-_putchar('%');
-count++;
-break;
-}
-}
-else
-{
-_putchar(*format);
-count++;
-}
-format++;
-}
-va_end(args);
-return (count);
+	for (; format[*i] == ' '; (*i)++)
+		if (format[*i + 1] == '\0')
+			return (0);
+	if (format[*i] == ' ')
+		(*i)++;
+	return (1);
 }
