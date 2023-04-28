3ecf412ded27addeedbe8fce84d6762ad72fbd7a
gloxoss <112545439+gloxoss@users.noreply.github.com>
2023-04-28 10:18:25 +0000

    Create get_precision.c

---
+++ get_precision.c
@@ -0,0 +1,42 @@
+#include <stdbool.h>
+#include <stdarg.h>
+
+/**
+ * get_precision - Parses the precision value from a format string.
+ * @format: The format string to parse.
+ * @start: The starting position in the format string to parse from.
+ * @ap: The va_list of arguments.
+ *
+ * Return: The precision value if found, or -1 if not found.
+ */
+int get_precision(const char *format, size_t start, va_list ap)
+{
+    int precision = -1;
+    bool has_precision = false;
+
+    for (size_t i = start; format[i] != '\0'; i++) {
+        if (format[i] == '.') {
+            has_precision = true;
+            continue;
+        }
+
+        if (has_precision) {
+            if (format[i] == '*') {
+                precision = va_arg(ap, int);
+                break;
+            }
+
+            if (format[i] >= '0' && format[i] <= '9') {
+                if (precision == -1) {
+                    precision = 0;
+                }
+
+                precision = precision * 10 + (format[i] - '0');
+            } else {
+                break;
+            }
+        }
+    }
+
+    return precision;
+}
